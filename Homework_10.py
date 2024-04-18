3532428a568c49d58101f0129f95ffe7b893d94b
GorbunovGHub <pavlixrus@mail.ru>
2024-04-18 16:10:57 +0300

    Add files via upload

--- Homework_10.py
+++ Homework_10.py
@@ -1,5 +1,5 @@
 cars_count = 0
-list_ = ['BMW', 'MB', 'LADA', 'HONDA']
+list_ = ['BMW', 'MB', 'LADA', 'kia', 'HONDA']
 for i in list_:
     cars_count += 10
     print('Я езжу на автомобиле марки ', i)
